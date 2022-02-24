84ce174442f62c4510434bd9bf493c4f0d4d5c50
pisarew <80489516+pisarew@users.noreply.github.com>
2022-02-24 17:28:45 +0300

    Update resurces.swift

--- VKApiApp/Resources/resurces.swift
+++ VKApiApp/Resources/resurces.swift
@@ -5,7 +5,7 @@
 //  Created by Глеб Писарев on 23.02.2022.
 //
 
-let token = "a6d0701fa6d0701fa6d0701fe0a6aa21cfaa6d0a6d0701fc77957373e0265f6e83e255d"
+let token = "KEY"
 let URL = "https://api.vk.com/method/wall.get"
 let version = 5.131
 
